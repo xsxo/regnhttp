0b6c3e0b7967c49dcc10b4230c2cf50ab93a2c32
amugslol <amugslol@gmail.com>
2026-01-17 17:01:02 +0300

    test v2.0.0 rem test connections

--- preparar_test.go
+++ preparar_test.go
@@ -10,7 +10,7 @@ var req *RequestType
 func prepare_request() {
 	req = Request()
 	req.SetMethod(MethodGet)
-	req.SetURL("https://localhost/api")
+	req.SetURL("https://httpbin.org/api")
 	req.Header.Set("Message1", "REGN HTTP v0.0.0-rc @xsxo - GitHub.com")
 	req.Header.Set("Message2", "REGN HTTP v0.0.0-rc @xsxo - GitHub.com")
 	req.SetBody([]byte("REGN HTTP TEST BODY"))
