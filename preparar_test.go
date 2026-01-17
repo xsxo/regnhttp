1cd47bf1cfa0fd048989b583409d7c91178b9dfc
amugslol <amugslol@gmail.com>
2026-01-17 22:12:03 +0300

    test yml file

--- preparar_test.go
+++ preparar_test.go
@@ -9,7 +9,7 @@ var req *RequestType
 
 func prepare_request() {
 	req = Request()
-	req.SetMethod(MethodGet)
+	req.SetMethod(MethodPost)
 	req.SetURL("https://localhost:11/api")
 	req.Header.Set("Message1", "REGN HTTP v0.0.0-rc @xsxo - GitHub.com")
 	req.Header.Set("Message2", "REGN HTTP v0.0.0-rc @xsxo - GitHub.com")
